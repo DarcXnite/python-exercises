33cf7f733f41c01be78738b09b48938427fc8a42
Davis Nguyen <davisnguyen@Daviss-MacBook-Air.local>
2022-07-05 16:57:49 -0500

    all challenges complete

--- exercises/01loop.py
+++ exercises/01loop.py
@@ -1,6 +1,11 @@
 # Write a function called `p_times` that takes a `statement` and
 # a `num` as inputs, and outputs the `statement` some `num` of times
 # to the console.
+def p_times(statement, num):
+    for i in range(0, num):
+        print(statement)
+
+p_times("We didn't learn this", 4)
 #
 # Example function call:
 #
--- exercises/05factorial.py
+++ exercises/05factorial.py
@@ -9,3 +9,11 @@
 #
 # > 120
 #
+
+def factorial(num):
+    product = 1
+    for i in range(1, num + 1):
+        product *= i
+    return product
+
+print(factorial(5))
